0156d00fed5d06cbe45772191ffbabf9deb4e03a
Shengyuan-Liu <sl5523@ic.ac.uk>
2024-10-16 14:42:57 +0100

    1

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -17,3 +17,20 @@ class Polynomial:
                 for d,c in enumerate(coefs[2:],start=2) if c]
         
         return "+".join(reversed(terms)) or "0"
+    
+    def __eq__(self, other):
+        return isinstance(other, Polynomial) and self.coefficients == other.coefficients
+    
+    def __add__(self, other):
+        if isinstance(other, Number):
+            return Polynomial((self.coefficients[0] + other,)+ self.coefficients[1:])
+        elif isinstance(other, Polynomial):
+            common = min(self.degree(), other.degree()) + 1
+            coefs = tuple(a + b for a, b in zip(self.coefficients[:common],other.coefficients[:common]))
+            coefs += self.coefficients[common:] + other.coefficients[common:]
+            return Polynomial(coefs)
+        else:
+            return NotImplemented
+        
+    def __radd__(self, other):
+        return self + other
